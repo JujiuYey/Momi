971258710dd133eefa34594350b2f251e66dde27
JujiuYey <1041556532@qq.com>
2026-04-24 16:02:38 +0800

    feat: add minimax auth opener command

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,6 +1,9 @@
+mod commands;
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
   tauri::Builder::default()
+    .plugin(tauri_plugin_opener::init())
     .setup(|app| {
       if cfg!(debug_assertions) {
         app.handle().plugin(
@@ -11,6 +14,9 @@ pub fn run() {
       }
       Ok(())
     })
+    .invoke_handler(tauri::generate_handler![
+      commands::provider_auth::open_provider_auth
+    ])
     .run(tauri::generate_context!())
     .expect("error while running tauri application");
 }
